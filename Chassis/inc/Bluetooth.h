19e2110d232140f223b71e605f1d96e677568447
Jesus Perez <119976553+CECS-Jesus@users.noreply.github.com>
2025-02-26 15:23:41 -0800

    Update Bluetooth.h

--- Chassis/inc/Bluetooth.h
+++ Chassis/inc/Bluetooth.h
@@ -7,12 +7,8 @@
 class Bluetooth {
 public:
     static void init();
-    static void uart0Init();
     static void uart1Init();
-    static void uart0OutChar(unsigned char data);
-    static void uart0OutString(unsigned char* pt);
     static unsigned char uart1InChar();
-    static void bltInString(unsigned char* bufPt);
 };
 
 #endif
